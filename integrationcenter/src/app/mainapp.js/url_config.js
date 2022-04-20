75bd0286923465e12e493a90ee01b4824e0984cf
pal276 <89001018+pal276@users.noreply.github.com>
2022-04-20 09:35:44 +0530

    Add ./integrationcenter/src/app/mainapp.js/url_config.js

--- integrationcenter/src/app/mainapp.js/url_config.js
+++ integrationcenter/src/app/mainapp.js/url_config.js
@@ -1,20 +1,21 @@
 "use strict";
 var app = angular.module("urlConfig", ["ngRoute", "ngGrid"]);
 app.service('CONFIG', function($location) {
-	console.log("config.js :: app.service :: Refering urls")
-	var Url = {
-		'projectAnalysis_url': 'http://localhost:8007',
-		'dashboard_url': '',
-		'codeReview_url': 'http://localhost:8001',
-		'Deployment_url': 'http://localhost:8010',
-		'onboarding_url': 'http://localhost:8008',
-		'estimation_url': 'http://localhost:8003',
-		'apiSpecs_url': 'http://localhost:8004',
-		'integrationcenter_url': 'http://localhost:8080',
-		'specAnalyzer_url': 'http://localhost:8006',
-		'inception_url': 'http://localhost:8989',
-		'logout_url': '',
-		'migrationcenter_url': 'http://localhost:8877'
-	};
-	return Url;
+ console.log("config.js :: app.service :: Refering urls")
+ var Url = {
+  'projectAnalysis_url': 'http://12.34.56.7777:8007',
+  'dashboard_url': '',
+  'codeReview_url': 'http://12.34.56.7777:8001',
+  'Deployment_url': 'http://12.34.56.7777:8010',
+  'onboarding_url': 'http://12.34.56.7777:8008',
+  'estimation_url': 'http://12.34.56.7777:8003',
+  'apiSpecs_url': 'http://12.34.56.7777:8004',
+  'integrationcenter_url': 'http://12.34.56.7777:8080',
+  'specAnalyzer_url': 'http://12.34.56.7777:8006',
+  'inception_url': 'http://12.34.56.7777:8989',
+  'logout_url': '',
+  'migrationcenter_url': 'http://12.34.56.7777:8877'
+ };
+ return Url;
 });
+
