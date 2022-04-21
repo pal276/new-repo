9aefa6ad88702b4e4283b2bfd7dd67999d3124a3
pal276 <89001018+pal276@users.noreply.github.com>
2022-04-21 15:15:51 +0530

    Added load_balancer_ip

--- integrationcenter/src/app/mainapp.js/url_config.js
+++ integrationcenter/src/app/mainapp.js/url_config.js
@@ -3,18 +3,18 @@ var app = angular.module("urlConfig", ["ngRoute", "ngGrid"]);
 app.service('CONFIG', function($location) {
        console.log("config.js :: app.service :: Refering urls")
        var Url = {
-              'projectAnalysis_url': 'http://35.202.92.47:8007',
+              'projectAnalysis_url': 'http://34.135.78.21:8007',
               'dashboard_url': '',
-              'codeReview_url': 'http://35.202.92.47:8001',
-              'Deployment_url': 'http://35.202.92.47:8010',
-              'onboarding_url': 'http://35.202.92.47:8008',
-              'estimation_url': 'http://35.202.92.47:8003',
-              'apiSpecs_url': 'http://35.202.92.47:8004',
-              'integrationcenter_url': 'http://35.202.92.47:8080',
-              'specAnalyzer_url': 'http://35.202.92.47:8006',
-              'inception_url': 'http://35.202.92.47:8989',
+              'codeReview_url': 'http://34.135.78.21:8001',
+              'Deployment_url': 'http://34.135.78.21:8010',
+              'onboarding_url': 'http://34.135.78.21:8008',
+              'estimation_url': 'http://34.135.78.21:8003',
+              'apiSpecs_url': 'http://34.135.78.21:8004',
+              'integrationcenter_url': 'http://34.135.78.21:8080',
+              'specAnalyzer_url': 'http://34.135.78.21:8006',
+              'inception_url': 'http://34.135.78.21:8989',
               'logout_url': '',
-              'migrationcenter_url': 'http://35.202.92.47:8877'
+              'migrationcenter_url': 'http://34.135.78.21:8877'
          };
          return Url;
 });
